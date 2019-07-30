7c38b33388b0ae68e864eb750d3d5b24f319033b
TangliziGit <tanglizimail@foxmail.com>
2019-07-30 11:49:16 +0800

    hdu-2829 accepted

--- training/2019/summerTraining/hdu/hdu-2829.cpp
+++ training/2019/summerTraining/hdu/hdu-2829.cpp
@@ -1,18 +1,35 @@
 #include <cstdio>
 #include <algorithm>
+#define debug(x) printf("%s == %d\n", #x, x)
 using namespace std;
 const int maxn=1e3+20;
 typedef long long LL;
 int n, m, fi, la, que[maxn];
 LL sum[maxn], cost[maxn], dp[maxn][maxn];
 
-LL X(int i, int n)  {return dp[i][n]+sum[i]*sum[i-1]-cost[i-1];}
-LL Y(int i)         {return sum[i];}
+LL Y(int i, int n)  {return dp[i][n]+sum[i]*sum[i+1]-cost[i+1];}
+LL X(int i)         {return sum[i];}
 
-LL gradUp(int i, int j)           {return Y(i)-Y(j);}
-LL gradDown(int i, int j, int n)  {return X(i, n)-X(j, n);}
-LL scost(int a, int b)            {return cost[b]-cost[a-1]-sum[a-1]*(sum[b]-sum[a]);}
-LL update(int i, int j, int n)    {return dp[j][n-1]+scost(j+1, i);}
+LL gradUp(int i, int j, int n)  {return Y(i, n)-Y(j, n);}
+LL gradDown(int i, int j)       {return X(i)-X(j);}
+LL scost(int a, int b)          {return cost[b]-cost[a]-sum[a-1]*(sum[b]-sum[a]);}
+LL update(int i, int j, int n)  {return dp[j][n-1]+scost(j+1, i);}
+
+bool judge(int i, int num){
+    LL down=gradDown(que[fi+1], que[fi]);
+    if (down>0)
+        return gradUp(que[fi+1], que[fi], num-1) <= sum[i]*down;
+    return gradUp(que[fi+1], que[fi], num-1) >= sum[i]*down;
+}
+
+bool ifUpdate(int i, int num){
+    int down1=gradDown(i, que[la]),
+        down2=gradDown(que[la], que[la-1]);
+
+    if (down1*down2>0)
+        return gradUp(que[la], que[la-1], num-1)*down1 >= gradUp(i, que[la], num-1)*down2;
+    return gradUp(que[la], que[la-1], num-1)*down1 <= gradUp(i, que[la], num-1)*down2;
+}
 
 int main(void){
     sum[0]=cost[0]=0;
@@ -31,13 +48,10 @@ int main(void){
             fi=la=0;
             que[la]=0;
             for (int i=1; i<=n; i++){
-                while (fi<la && gradUp(que[fi+1], que[fi]) <= sum[i]*gradDown(que[fi+1], que[fi], num-1))
-                    fi++;
+                while (fi<la && judge(i, num)) fi++;
                 dp[i][num]=update(i, que[fi], num);
-                while (fi<la && gradUp(que[la], que[la-1])*gradDown(i, que[la], num-1) >=
-                        gradUp(i, que[la])*gradDown(que[la], que[la-1], num-1))
-                    la--;
-                que[--la]=i;
+                while (fi<la && ifUpdate(i, num)) la--;
+                que[++la]=i;
             }
         }
         printf("%lld\n", dp[n][m+1]);
